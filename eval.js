1616bcfd1692006547043d36c86e02c42bcc15ce
Michael Welsman-Dinelle <michael@cohodata.com>
2015-03-18 18:55:04 -0700

    Get the recursion down a little more. Still not quite right, since it fails on some mixed s-exp/identifier combos

--- eval.js
+++ eval.js
@@ -10,7 +10,9 @@
 	    // or a character from the extended set, !$%&*+-./:<=>?@^_~
 	    // Digits 0-9 may be used after the first character
 	    identifier: /^[a-zA-Z!$%&*+-./:<=>?@^_~]+[a-zA-Z0-9!$%&*+-./:<=>?@^_~]$/,
+	    singleLevelParen: /\([^)]*\)/g, // one level of paren matching
 	    comment: /;/
+
 	}
     };
     context[globalName] = r7rs;
@@ -23,6 +25,35 @@
 
     r7rs.tokenize = function tokenize(s) {
 	return s.match(/\S+/g);
+    };
+
+    // Find identifiers and s-expressions
+    r7rs.superTokenize = function superTokenize(exp) {
+	var sRegex = new RegExp(r7rs.regex.singleLevelParen);
+	var match, indices = [];
+	while ((match = sRegex.exec(exp))) {
+	    indices.push([match.index, match.index+match[0].length]);
+	}
+	if(!indices.length) {
+	    return [exp]; // one big token
+	}
+	var last = 0, result = [];
+	indices.forEach(function(indexRange) {
+	    if(last < indexRange[0]) {
+		result.push(exp.substr(last, indexRange[0] - last));
+	    }
+	    result.push(exp.substr(indexRange[0], indexRange[1]));
+	    last = indexRange[1];
+	});
+	return result;
+    };
+
+    r7rs.stripExpression = function strip(sexp) {
+	var s = sexp.trim().replace('\n', '');
+	if(s[0] !== '(' || s[s.length - 1] !== ')') {
+	    return undefined;
+	}
+	return s.substr(1, s.length - 2);
     };
 
     r7rs.matchParen = function matchParen(sexp) {
@@ -44,20 +75,27 @@
     };
 
     r7rs.treeify = function treeify(exp) {
-	// TODO: could be other built-ins, etc.
+	if(!exp.trim()) {
+	    console.log('empty', exp);
+	    return null; // empty
+	}
 	if(r7rs.isIdentifier(exp)) {
+	    console.log('found an identifier', exp);
 	    return exp;
 	} else {
-	    if(exp[0] !== '(' || exp[exp.length - 1] !== ')') {
-		throw exp;
+	    var s = r7rs.stripExpression(exp);
+	    console.log('interpreting as expression with contents', s);
+
+	    if(!s) {
+		throw 'Neither animal, mineral, nor vegetable';
 	    }
 
+	    // FIXME: doesn't handle recursion with some s-exps, some identifiers
 	    var result = [];
-	    // FIXME: tokenize isn't enough to break down into s-exps! It only works inside
-	    // of a known s-exp!
-	    this.tokenize(exp.substr(1, exp.length - 2)).forEach(function(token) {
+	    r7rs.superTokenize(s).forEach(function(token) {
 		result.push(treeify(token));
 	    });
+	    console.log('treeify result', result);
 	    return result;
 	}
     };
