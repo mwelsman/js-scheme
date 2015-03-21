b608a545dc5763eff5ee4fadda9871dcd28b3571
Michael Welsman-Dinelle <michael@cohodata.com>
2015-03-21 12:49:23 -0700

    Remove unused functions and make eval() operate directly on a string

--- eval.js
+++ eval.js
@@ -35,19 +35,29 @@
 	return r7rs.regex.identifier.exec(s) !== null;
     };
 
-    r7rs.eval = function eval(list) {
-	var args = list.slice(1).map(function convert(item) {
-	    if (Array.isArray(item)) {
-		return eval(item);
-	    } else {
-		// Perform number conversion
-		return Number(item[0]) ? Number(item): item;
-	    }
-	});
-	return r7rs.builtIns[list[0]].apply(null, args);
+    /*
+     * Constructs an AST out of a string and then evaluates it
+     */
+    r7rs.eval = function eval(str) {
+	var tree = r7rs.tree(str);
+
+	return (function recursiveEval(list) {
+	    var args = list.slice(1).map(function convert(item) {
+		if (Array.isArray(item)) {
+		    return recursiveEval(item);
+		} else {
+		    // Perform number conversion
+		    return Number(item[0]) ? Number(item): item;
+		}
+	    });
+	    return r7rs.builtIns[list[0]].apply(null, args);
+	})(tree);
     };
 
-    // Build a tree out of nested s-expressions
+    /*
+     * Constructs an AST out of a string consisting of numbers,
+     * string literals, and potentially nested s-expressions
+     */
     r7rs.tree = function tree(s) {
 	var cur = undefined;
 	var append = false;
@@ -83,90 +93,4 @@
 	cur.shift();
 	return cur;
     };
-
-    // Find one level of identifiers and s-expressions
-    r7rs.superTokenize = function superTokenize(exp) {
-	var sRegex = new RegExp(r7rs.regex.singleLevelParen);
-	var match, indices = [];
-	while ((match = sRegex.exec(exp))) {
-	    indices.push([match.index, match.index+match[0].length]);
-	}
-	if(!indices.length) {
-	    return [exp]; // one big token
-	}
-	var last = 0, result = [];
-	indices.forEach(function(indexRange) {
-	    if(last < indexRange[0]) {
-		result.push(exp.substr(last, indexRange[0] - last).trim());
-	    }
-	    result.push(exp.substr(indexRange[0], indexRange[1]).trim());
-	    last = indexRange[1];
-	});
-	return result.filter(function(token) { return !!token; });
-    };
-
-    r7rs.stripExpression = function strip(sexp) {
-	var s = sexp.trim().replace('\n', '');
-	if(s[0] !== '(' || s[s.length - 1] !== ')') {
-	    return undefined;
-	}
-	return s.substr(1, s.length - 2);
-    };
-
-    r7rs.matchParen = function matchParen(sexp) {
-	var sum = 0;
-	sexp.split('').forEach(function(c) {
-	    if (c === '(') {
-		sum++;
-	    } else if (c === ')') {
-		sum--;
-	    }
-
-	    if(sum < 0) {
-		throw 'Closed too many parens';
-	    }
-	});
-	if(sum > 0) {
-	    throw 'Failed to close all parens';
-	}
-    };
-
-    r7rs.treeify = function treeify(exp) {
-	if(!exp.trim()) {
-	    console.log('empty', exp);
-	    return null; // empty
-	}
-	if(r7rs.isIdentifier(exp)) {
-	    console.log('found an identifier', exp);
-	    return exp;
-	} else {
-	    var s = r7rs.stripExpression(exp);
-	    console.log('interpreting as expression with contents', s);
-
-	    if(!s) {
-		throw 'Neither animal, mineral, nor vegetable';
-	    }
-
-	    // FIXME: doesn't handle recursion with some s-exps, some identifiers
-	    // FIXME: whitespace needs to be stripped so it doesn't result in more
-	    //        nodes!
-	    var result = [];
-	    r7rs.superTokenize(s).forEach(function(token) {
-		result.push(treeify(token));
-	    });
-	    console.log('treeify result', result);
-	    return result;
-	}
-    };
-
-    r7rs.execute = function execute(sexp) {
-	// treeify
-	// walk tree and call
-	// eval('');
-	// built-ins?
-    };
-
-    // Constructor for a new evaluation context
-    function Context() {
-    }
 })(window, 'r7rs');
